1ae91ab87ce9d76c88ae051073df1fb9ac64c6c0
Tony Mishler <tony.mishler@gmail.com>
2023-03-13 21:26:00 -0500

    logging

--- lua/create-efr/dev.lua
+++ lua/create-efr/dev.lua
@@ -7,9 +7,10 @@ local function create_ef_resource()
   -- Determine the project root directory
   local csproj_file = nil
   local dir = vim.fn.getcwd()
+  print('test')
   print('dir:', dir)
   local parent_dir = vim.fn.fnamemodify(dir, ':h')
-  print('parent_dir:', parent_dir)
+  
   local count = 0
   while dir ~= nil and count < 2 do
     csproj_file = vim.fn.findfile('*.csproj', dir .. ';')
